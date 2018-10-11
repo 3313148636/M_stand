9fdddd82286024043010e93d622780f70f9a85f2
3313148636 <3313148636@qq.com>
2018-10-11 15:33:27 +0800

    project initial

---
+++ src/javascripts/app.js
@@ -0,0 +1,23 @@
+
+/*
+const Router = require('./router');
+
+const home_controller = require('./controllers/home_controller');
+
+// 渲染视图
+home_controller.render(); 
+
+// 启动路由
+const router = new Router({ initial: '#/job' });
+window.router = router;
+router.init();
+*/
+
+// const home_job_model = require('./models/home_job_model');
+
+// var getData = async ()=>{
+//     var data = await home_job_model.job_list();
+//     return data;
+// }
+
+// console.log(getData());
